aec4a9ebd9ddf7bff69c2f7befd3982466a5aaad
MD Nafizul Iqram <nafizulikram3@gmail.com>
2025-01-16 00:43:45 +0600

    fetched all articles data

--- src/pages/AllArticles.jsx
+++ src/pages/AllArticles.jsx
@@ -1,8 +1,37 @@
+import useAxiosPublic from "@/hooks/useAxiosPublic";
+import Loader from "@/shared/LoaderSpinner";
+import { useQuery } from "@tanstack/react-query";
 
 const AllArticles = () => {
+    const axiosPublic = useAxiosPublic()
+
+    const { data=[], isLoading } = useQuery({
+        queryKey: ["articles"],
+        queryFn: async () => {
+            const { data } = await axiosPublic.get("/all-articles")
+            return data
+        }
+    })
+    const approvedArticles = data.filter(article => article.status === "approved")
+    console.log(approvedArticles)
     return (
         <div>
-            All Articles
+            {isLoading ?
+                <div className="flex items-center justify-center gap-3 h-screen">
+                    <Loader></Loader> <h1 className="text-4xl">Loading</h1></div>
+                :
+                <div>
+                    {/* search filter section */}
+                    <nav>
+                        {/* search section */}
+                        <div>
+                            <input type="text" />
+                        </div>
+                        
+                        <div></div>
+                    </nav>
+                </div>
+            }
         </div>
     );
 };
