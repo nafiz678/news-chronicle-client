77904c1bbdff85a0fb5885f74225c684ce42433e
MD Nafizul Iqram <nafizulikram3@gmail.com>
2025-01-15 14:46:26 +0600

    fixing some errors

--- src/firebase/firebase_init.js
+++ src/firebase/firebase_init.js
@@ -1,17 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDZql7iJk5Axrrr_o09KAxRyDMQiiizrHs",
-  authDomain: "news-chronical.firebaseapp.com",
-  projectId: "news-chronical",
-  storageBucket: "news-chronical.firebasestorage.app",
-  messagingSenderId: "586758523933",
-  appId: "1:586758523933:web:eec32bba3e660b5a5d0af4",
-};
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+}
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
